cd4379963b49272e8a86eb7bddc699e5913f175e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-14 07:21:21 +0000

    Deploy GSS-FED/vital-ui-kit to github.com/GSS-FED/vital-ui-kit.git:master

--- docs/css/styleguide.css
+++ docs/css/styleguide.css
@@ -1,7 +1,7 @@
 /*******************************************
  * Copyright © 2020 Galaxy Software Services
  *
- * @gssfed/vital-ui-kit, v1.0.1
+ * @gssfed/vital-ui-kit, v1.0.3
  * UI Kit for GSS Vital Family
  *
  * By Neil Lin (https://github.com/Neil-Lin),Patric,Laura Lee (https://github.com/l443018),Eric Yip (https://github.com/ericyip),Vibrissa (https://github.com/Vibrissa),YuRu Lee (https://github.com/YuRu-Lee),CJies Tan (https://github.com/cjies),Evan Wu (https://github.com/evanwu-tw)
